3679464ac4cc5139e5f336a74cacfc593ee7588d
shengbin <shengbin.wang@baifendian.com>
2019-04-25 13:38:31 +0800

    micro spring boot service

---
+++ micro-spring-boot-service/src/main/java/micro/spring/boot/SpringBootServiceMain.java
@@ -0,0 +1,17 @@
+package micro.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: shengbin
+ * @since: 2019/4/21 上午11:07
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringBootServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootServiceMain.class, args);
+    }
+}
