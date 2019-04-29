4a914514dfa8958a37a17293f09a97422cae78ff
shengbin <shengbin.wang@baifendian.com>
2019-04-29 16:00:09 +0800

    zuul gateway dependences

---
+++ micro-spring-cloud-zuul/src/main/java/micro/spring/zuul/MicroSpringZuulMain.java
@@ -0,0 +1,19 @@
+package micro.spring.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关路由
+ *
+ * @Author: shengbin
+ * @since: 2019/4/29 下午2:09
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class MicroSpringZuulMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroSpringZuulMain.class, args);
+    }
+}
