da3ed16c255875a95867d5d347864962205c8738
shengbin <shengbin.wang@baifendian.com>
2019-07-14 14:36:53 +0800

    syn

--- micro-spring-cloud-ribbon-consumer/src/main/java/micro/spring/cloud/ribbon/RibbonConsumerMain.java
+++ micro-spring-cloud-ribbon-consumer/src/main/java/micro/spring/cloud/ribbon/RibbonConsumerMain.java
@@ -4,11 +4,6 @@ package micro.spring.cloud.ribbon;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 /**
  * 1)EnableDiscoveryClient: find Service
@@ -18,17 +13,17 @@ import org.springframework.web.client.RestTemplate;
  * @since: 2019/4/21 下午5:12
  */
 @EnableSwagger2Doc
-@EnableCircuitBreaker
-@EnableDiscoveryClient
+//@EnableCircuitBreaker
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class RibbonConsumerMain {
 
     // 客户端负载均衡
-    @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+//    @Bean
+//    @LoadBalanced
+//    RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(RibbonConsumerMain.class, args);
---
+++ micro-spring-cloud-test/src/test/java/ObservableTest.java
@@ -0,0 +1,9 @@
+
+/**
+ * @Author: shengbin
+ * @since: 2019/7/13 下午10:06
+ */
+public class ObservableTest  {
+
+
+}
