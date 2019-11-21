0caf6983160312d31fb7828372bc7d7c3c261c25
shengbin <shengbin.wang@baifendian.com>
2019-11-21 18:25:36 +0800

    add cache config

--- springCloud-caffeine/src/main/java/micro/spring/cloud/caffeine/controller/CaffeineController.java
+++ springCloud-caffeine/src/main/java/micro/spring/cloud/caffeine/controller/CaffeineController.java
@@ -5,6 +5,7 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import lombok.extern.slf4j.Slf4j;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,9 @@ import java.util.concurrent.TimeUnit;
 @RestController("/caffeine")
 @Slf4j
 public class CaffeineController {
+
+    @Autowired
+    private Cache cache;
 
     @GetMapping(value = "/getValueLoad")
     public void getValueFromCaffeineLoad(@RequestParam(value = "key") String key) {
@@ -113,4 +117,47 @@ public class CaffeineController {
 
         System.out.println(cache.getIfPresent(keyName));
     }
+
+
+    @GetMapping(value = "/getValueManualCountByConfig")
+    public void getValueFromCaffeineManualByConfig(@RequestParam(value = "key") String keyName) {
+
+        Integer age1 = (Integer) cache.getIfPresent(keyName);
+        System.out.println(age1);
+
+        for (int i = 0; i < 10; i++) {
+            @Nullable Integer count = (Integer) cache.getIfPresent(i + "");
+            if (null == count) {
+                cache.put("" + i, 0);
+            }
+        }
+        log.info("cache1 :{}", cache.asMap());
+
+        for (int i = 4; i < 11; i++) {
+            @Nullable Integer count = (Integer) cache.getIfPresent(i + "");
+            if (null == count) {
+                cache.put("" + i, 0);
+            } else {
+                // 当key不存在时，会立即创建出对象来返回，age2不会为空
+                Integer age2 = (Integer) cache.get(i + "", k -> {
+                    System.out.println("默认值:" + k);
+                    return 0;
+                });
+                cache.put("" + i, age2 + 1);
+            }
+        }
+        log.info("cache2 :{}", cache.asMap());
+
+        System.out.println(cache.getIfPresent(keyName));
+    }
+
+    @GetMapping(value = "/teest")
+    public void getTest(@RequestParam(value = "key") String keyName) {
+        Integer age1 = (Integer) cache.getIfPresent(keyName);
+        System.out.println("value:" + age1);
+
+        log.info("cache config :{}", cache.asMap());
+
+    }
+
 }
