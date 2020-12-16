d1a81832855804eb4ebb37e91c74a3e3d0fc6f9d
shengbin <shengbin.wang@baifendian.com>
2020-12-16 23:36:05 +0800

    circle queue

--- algorithm/src/main/java/algorithm/queue/LiinkedQueue.java
+++ algorithm/src/main/java/algorithm/queue/LiinkedQueue.java
@@ -6,12 +6,20 @@ package algorithm.queue;
  */
 public class LiinkedQueue {
     public static void main(String[] args) {
-        CircleLinkedQueue circleLinkedQueue = new CircleLinkedQueue(3);
+        CircleLinkedQueue circleLinkedQueue = new CircleLinkedQueue(6);
         circleLinkedQueue.printAll();
+
         circleLinkedQueue.enQueue("a");
         circleLinkedQueue.enQueue("b");
         circleLinkedQueue.enQueue("c");
         circleLinkedQueue.enQueue("d");
+        circleLinkedQueue.printAll();
+
+        System.out.println("出队列：" + circleLinkedQueue.deQueue());
+        circleLinkedQueue.enQueue("c");
+        System.out.println("出队列：" + circleLinkedQueue.deQueue());
+        circleLinkedQueue.enQueue("d");
+        circleLinkedQueue.enQueue("e");
         circleLinkedQueue.printAll();
     }
 }
@@ -37,16 +45,32 @@ class CircleLinkedQueue {
         return true;
     }
 
-    public void deQueue() {
-
+    public String deQueue() {
+        // 队列为空的条件
+        if (tail == head) {
+            return null;
+        }
+        String deStr = items[head];
+        head = (head + 1) % n;
+        return deStr;
     }
 
+    /**
+     * 按照出队的顺序打印
+     */
     public void printAll() {
         System.out.println("head=" + head);
         System.out.println("tail=" + tail);
-        for (int i = head; i < tail; i++) {
-            System.out.println(items[i]);
+        if (tail != head) {
+            int headTemp = head;
+            // 队列满
+            while (tail != (headTemp + 1) % n) {
+                System.out.println(items[headTemp]);
+                headTemp = (headTemp + 1) % n;
+            }
+            System.out.println(items[headTemp]);
         }
+
         System.out.println("===================================");
     }
 }
